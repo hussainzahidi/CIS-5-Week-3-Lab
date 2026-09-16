e667e48b61f43ec700b626d0565d9b31b58055e3
hussainzahidi <m.h.zahidi@outlook.com>
2026-09-15 18:17:15 -0700

    intialized variables and printed output

--- main.cpp
+++ main.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
 #include <string>
 
-// Lab 3 — Your Name
+// Lab 3 — Hussain Zahidi
 // CIS 5 Week 03 · Types & variables
 
 int main() {
+	using std::cin;
+	using std::cout;
+	using std::string;
+	using std::endl;
+
   const int CURRENT_YEAR = 2026;
+  string first_name = "Hussain";
+  int age = 26;
+  double height = 1.75; // in meters
+  char first_initial = 'H';
+  bool isStudent = true;
 
-  // TODO: declare and initialize on the same line
-  // std::string name = ...;
-  // int age = ...;
-  // double height_m = ...;
-  // char initial = ...;
-  // bool student = ...;
 
-  std::cout << "=== About me ===\n";
-  // TODO: labeled prints from the variable names + CURRENT_YEAR
-
+  cout << "=== About me ===" << endl;
+  cout << "The year is : " << CURRENT_YEAR << endl;
+  cout << "Name : " << first_name << endl;
+  cout << "Age : " << age << endl;
+  cout << "Height : " << height << " meters" << endl;
+  cout << "First initial : " << first_initial << endl;
+  cout << "Student : " << isStudent << endl;
   return 0;
 }
